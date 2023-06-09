4ef12f5de402afb4207fbbef47f2c5b77c4e07e6
Christian Martinez <christian@mixdlabs.com>
2023-06-09 09:56:50 -0600

    In our app, the licenses prompt is a boolean with two choices - MIT or ISC. Given that data, we only need a simple license function to render the license out in markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,34 @@
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+const renderLicense = (license) => {
   license === 'MIT'
-    ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
-    : license === 'ISC'
-    ? '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
-    : ''
+    ? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    : '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
 }
 
-// Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  license === 'MIT'
-    ? '(https://opensource.org/licenses/MIT)'
-    : license === 'ISC'
-    ? '(https://opensource.org/licenses/ISC)'
-    : ''
+// Create a function that returns a default contributing section
+const renderContributing = () => {
+  return `## Contributing
+
+    Pull requests are welcome. For major changes, please open an issue first
+    to discuss what you would like to change.
+    
+    Please make sure to update tests as appropriate.`
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  license
-    ? `
-    ${renderLicenseBadge}${renderLicenseLink}
-    `
-    : ''
+// Create a function that returns a markdown image
+const renderImage = (imgUrl) => {
+  return `![alt text](${imgUrl})`
 }
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README file
 function generateMarkdown(data) {
-  return `# ${data.title}
-  ${data.description}  
-
+  return `
+  ${data.image ? renderImage(data.imageUrl) : ''}
+  # ${data.title}
+  ${data.description}
+  ${data.contributing ? renderContributing() : ''}  
+${renderLicense(data.license)}
 `
 }
 
